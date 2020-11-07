f26f064f97f9a338ce96a0c1654815747c37a252
Rogerio Matoso <rogeriomatoso@yahoo.com.br>
2020-11-06 21:31:14 -0300

    Inicio das camadas Dominio e Dadoos. Criação das classes e atributos: Montadora e Veiculo

---
+++ Montadora.Veiculos.Dominio/Montadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montadora.Veiculos.Dominio
+{
+    public class Montadora
+    {
+        public int Id {get;set;}
+        public string Nome { get; set;}
+        public string Nacionalidade { get; set; }
+        public string MercadoPaises { get; set; }
+        public virtual List<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ Montadora.Veiculos.Dominio/Veiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montadora.Veiculos.Dominio
+{
+    public class Veiculo
+    {
+        public long IdVeiculo { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public virtual Montadora montadora { get; set; }
+        public int IdMontadora { get; set; }
+    }
+}
