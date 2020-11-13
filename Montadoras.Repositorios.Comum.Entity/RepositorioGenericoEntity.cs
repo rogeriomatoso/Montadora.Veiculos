291268d0a85e675c125f40a5c5ee2bfe81eb7b99
Rogerio Matoso <rogeriomatoso@yahoo.com.br>
2020-11-13 12:38:14 -0300

    Camada Repositorio Comum criada; Interface repositorio criada; Camada Repositorio.Entity criada; Repositorio Generrico criado;

---
+++ Montadoras.Repositorios.Comum.Entity/RepositorioGenericoEntity.cs
@@ -0,0 +1,58 @@
+﻿using Montadoras.Veiculos.Repositorios.Comum;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montadoras.Repositorios.Comum.Entity
+{
+    public class RepositorioGenericoEntity<TEntidade, TChave> : IRepositorioGenerico<TEntidade, TChave>
+        where TEntidade : class
+    {
+        private DbContext _contexto;
+
+        public RepositorioGenericoEntity(DbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Alterar(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Attach(entidade);
+            _contexto.Entry(entidade).State = EntityState.Modified;
+            _contexto.SaveChanges();
+        }
+
+        public void Excluir(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Attach(entidade);
+            _contexto.Entry(entidade).State = EntityState.Deleted;
+            _contexto.SaveChanges();
+        }
+
+        public void ExcluirPorId(TChave id)
+        {
+            TEntidade entidade = SelecionarPorId(id);
+            Excluir(entidade);
+        }
+
+        public void Inserir(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Add(entidade);
+            _contexto.SaveChanges();
+        }
+
+        public List<TEntidade> Selecionar()
+        {
+            return _contexto.Set<TEntidade>().ToList();
+        }
+
+        public TEntidade SelecionarPorId(TChave id)
+        {
+            return _contexto.Set<TEntidade>().Find(id);
+        }
+    }
+
+}
